64f2eacb0022704907525442123da280ceb94ee7
Malik <malik.h@webdevpro.net>
2022-05-16 20:11:30 +0200

    fin live-1

---
+++ live-1/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.p}>j'ai réussi à utiliser
+        l'émulateur android
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  p : {
+    color : "#7FB5FF",
+    fontSize : 30
+  }
+});
